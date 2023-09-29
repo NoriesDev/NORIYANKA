881af131af5eb45a903e2b067649c70aaae6a5b1
NoriesDev <noriesdevelopment@gmail.com>
2023-09-29 15:09:50 +0200

    changing from http to https

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
 
   const apiUrl = async () => {
     try{
-    const url = await fetch("http://hn.algolia.com/api/v1/search");
+    const url = await fetch("https://hn.algolia.com/api/v1/search");
     const dataFromApi = await url.json();
     setData(dataFromApi.hits);
     setLoading(false);
